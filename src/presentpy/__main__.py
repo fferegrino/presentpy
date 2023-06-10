0d976897322351b197022f0426bb69f7dad6a950
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-06-10 22:30:36 +0100

    Add ruff

--- src/presentpy/__main__.py
+++ src/presentpy/__main__.py
@@ -1,8 +1,12 @@
 import click
+from pathlib import Path
 
 @click.command()
-def run_presentpy():
-    print('Hello, world!')
+@click.argument('notebook', type=click.Path(exists=True))
+def run_presentpy(notebook):
+    notebook_path = Path(notebook)
+
+    print(f'Running {notebook_path}...')
 
 if __name__ == '__main__':
     run_presentpy()
