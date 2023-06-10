cc51c18a5e0c202acab69cf4feac4d3ddd1e66d0
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-06-10 22:03:44 +0100

    Initial setup

---
+++ readme.md
@@ -0,0 +1,4 @@
+PresentPy
+=========
+
+Create slides from Jupyter Notebooks
---
+++ src/presentpy/__main__.py
@@ -0,0 +1,8 @@
+import click
+
+@click.command()
+def run_presentpy():
+    print('Hello, world!')
+
+if __name__ == '__main__':
+    run_presentpy()
